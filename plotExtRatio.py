76c8b7aeb5a7cfd9013ee9371fe757eff21ee5d7
s1dav1s <s1dav1s@stanford.edu>
2020-01-02 12:48:31 -0800

    Updated comments on plot code

--- plotExtRatio.py
+++ plotExtRatio.py
@@ -1,6 +1,5 @@
-#This plots data from the Vav column of a table in our database.
+#This plots Alice's extinction ratio over time.
 #It retrieves data stored from the corresponding table in the database and then plots the data.
-#For more detailed comments, see plot_36hours.py
 #Requirements: Python3, mysql, packages listed below
 
 import numpy as np
@@ -10,10 +9,11 @@ import datetime
 import math
 import pymysql
 import os
+import matplotlib as mpl
 
 #Start and end datetimes that define range of data to retrieve from mysql table
-START_TIME = '2019-12-09 16:00:00'
-END_TIME = '2019-12-09 16:10:00'
+START_TIME = '2019-12-09 17:03:08'
+END_TIME = '2019-12-09 18:00:00'
 
 
 TABLE_NAME = 'ratiorecord'
@@ -21,7 +21,7 @@ TABLE_NAME = 'ratiorecord'
 
 #Connect to mysql database
 db = pymysql.connect(host="192.168.0.125",  #IP of computer with database. Local host if is same computer.
-					 user="inqnet4", #username
+					 user="inqnet1", #username
 					 passwd="Teleport1536!",  # your password
 					 db="extinctionR", #name of the database
 					 charset='utf8mb4',
@@ -33,28 +33,35 @@ datetimeArr=[]
 vmaxArr=[]
 vavArr=[]
 
+mpl.rcParams["savefig.directory"] = os.chdir(os.path.dirname("/home/inqnet1/Desktop/CQNET/CQNET-Interf"))
+
+
 try:
 	#Create cursor to select data from mysql.
 	with db.cursor() as cur:
 		#Mysql command to select data from each column
 		query = "SELECT vmax,vav,datetime FROM "+TABLE_NAME+" WHERE datetime BETWEEN {ts %s} AND {ts %s}"
-		#Execute query and store data in array from Vav column
+		#Execute query, then store data from columns in arrays
 		cur.execute(query,(START_TIME,END_TIME,))
-		row = cur.fetchone()
-		while row is not None:
+		row = cur.fetchone() #Retrieves row of table at START_TIME
+		while row is not None: #Iterates through valid rows
 			vmax = row["vmax"]
-			vmaxArr.append(vmax)
+			vmaxArr.append(1000*vmax)
 			vav = row["vav"]
-			vavArr.append(vav)
-			eRat = -10*np.log(vav/vmax)
+			vavArr.append(1000*vav)
+			eRat = -10*np.log10(vav/vmax)
 			ExRatioArr.append(eRat)
 			datetimeArr.append(row["datetime"])
 			row = cur.fetchone()
 finally: #Once store the data of each column in separate arrays, close the database
 	db.close()
 
+vmaxArr=np.array(vmaxArr)
+vavArr=np.array(vavArr)
+ExRatioArr=np.array(ExRatioArr)
+datetimeArr=np.array(datetimeArr)
 
-#Get the first and last datetimes of the Vav data. The retrieved data falls within the
+#Get the first and last datetimes of the extinction ratio data. The retrieved data falls within the
 #bounds of the start and end times that were specified, but may not be exactly the same.
 datetime_first = str(datetimeArr[0])
 datetime_last = str(datetimeArr[-1])
@@ -62,7 +69,7 @@ first_time = datetime.datetime.strptime(datetime_first,'%Y-%m-%d %H:%M:%S')
 datetime_dt = []
 datetime_el = []
 
-#Create the elapsed time array for Vav
+#Create the elapsed time array for extinction ratio
 for t in datetimeArr:
 	t=str(t)
 	datime=datetime.datetime.strptime(t,'%Y-%m-%d %H:%M:%S')
@@ -79,11 +86,14 @@ axs[0].set_ylabel(r"Ext Ratio")
 axs[0].grid()
 #Vin Plot
 axs[1].plot(datetime_el, vavArr,  linestyle = 'none', marker = '.', markersize = 2)
-axs[1].set_ylabel(r"$V_{min,av}$ (V)")
+axs[1].set_ylabel(r"$V_{min,av}$ (mV)")
 axs[1].grid()
 #Phase Plot
 axs[2].plot(datetime_el, vmaxArr,  linestyle = 'none', marker = '.', markersize = 2)
-axs[2].set_ylabel(r"$V_{max}$ (V)")
+axs[2].set_ylabel(r"$V_{max}$ (mV)")
 axs[2].grid()
-fig.suptitle("Alice's Pulse Extinction Ratio from \n"+str(time_Vap[0]+datetime.timedelta(minutes=xmin1))+" to "+str(time_Vap[0]+datetime.timedelta(minutes=xmax1)))
+xmin1=0
+xmax1=0
+fig.suptitle("Alice's Pulse Extinction Ratio from \n"+str(datetimeArr[0]+datetime.timedelta(minutes=xmin1))+" to "+str(datetimeArr[-1]+datetime.timedelta(minutes=-xmax1)))
 plt.xlabel('Elapsed time (min)', fontsize =16)
+plt.show()
